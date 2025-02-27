f2820e2a19adff528d7932776895cbc6ab291cbf
INotGreen <1691587386@qq.com>
2025-02-27 14:17:12 +0800

    makew

--- ImplantGo/Helper/function/Function.go
+++ ImplantGo/Helper/function/Function.go
@@ -1,12 +1,10 @@
 package Function
 
 import (
-	"encoding/binary"
 	"io/ioutil"
 	"main/Encrypt"
 	"main/MessagePack"
 	"main/PcInfo"
-	"net"
 	"strings"
 
 	"github.com/togettoyou/wsc"
@@ -60,46 +58,4 @@ func SendData(data []byte, Connection *wsc.Wsc) {
 		return
 	}
 	Connection.SendBinaryMessage(endata)
-}
-
-func TcpSend(msg []byte, conn net.Conn) {
-	defer func() {
-		if err := recover(); err != nil {
-			//log.Println("Send error:", err)
-		}
-	}()
-
-	if conn == nil {
-		//log.Println("Connection not established")
-		return
-	}
-	msg, err := Encrypt.Encrypt(msg)
-	if err != nil {
-		return
-	}
-	bufferSize := int32(len(msg))
-	err = binary.Write(conn, binary.LittleEndian, bufferSize)
-	if err != nil {
-		//log.Println("Failed to send buffer size:", err)
-		return
-	}
-
-	const chunkSize = 50 * 1024 // 50 KB
-	var chunk []byte
-
-	for bytesSent := 0; bytesSent < len(msg); {
-		if len(msg)-bytesSent > chunkSize {
-			chunk = msg[bytesSent : bytesSent+chunkSize]
-		} else {
-			chunk = msg[bytesSent:]
-		}
-
-		_, err := conn.Write(chunk)
-		if err != nil {
-			//log.Println("Failed to send data:", err)
-			return
-		}
-
-		bytesSent += len(chunk)
-	}
 }
